7db89633b6f316cddbd116a5202fd8ffc9987878
Mijryanbitao <mijryanb@gmail.com>
2024-09-10 09:58:40 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// create a variable named firstFavNum and store a favorite number 
+let firstFavNum = 69;
+
+// create a variable named secondFavNum and store a favorite number
+let secondFavNum = 96;
+
+// Perform arithmetic operation
+console.log("Addition:", firstFavNum + secondFavNum);
+console.log("Subtraction:", firstFavNum - secondFavNum);
+console.log("Multiplication:", firstFavNum * secondFavNum);
+console.log("Division:", firstFavNum / secondFavNum);
+console.log("Modulos:", firstFavNum % secondFavNum);
+console.log("Exponentiation (power):", firstFavNum ** secondFavNum);
